b8caa3821ceff355edda7a4103d2cbfe263c68f2
Kevin Bermudez <kevinbermudezc@gmail.com>
2025-09-09 16:19:37 -0500

    refactor: improve formatting of fun_insert_usuario function for better readability

--- db/fun_insert_usuario.sql
+++ db/fun_insert_usuario.sql
@@ -1,15 +1,16 @@
 CREATE OR REPLACE FUNCTION fun_insert_usuario(
-    p_cuenta tab_usuario.cuenta%type,
-    p_tipo tab_usuario.tipo%type,
-    p_clave tab_usuario.clave%type,
+  p_cuenta tab_usuario.cuenta%type,
+  p_tipo tab_usuario.tipo%type,
+  p_clave tab_usuario.clave%type,
 	p_dinero tab_usuario.dinero%type,
-    p_nombre tab_usuario.nombre%type,
-    p_apellido tab_usuario.apellido%type
+  p_nombre tab_usuario.nombre%type,
+  p_apellido tab_usuario.apellido%type
 )
 RETURNS int
 AS $$
 BEGIN
-    INSERT INTO tab_usuario (cuenta, tipo, clave, dinero, nombre, apellido) values (p_cuenta, p_tipo, p_clave, p_dinero, p_nombre, p_apellido);
+  INSERT INTO tab_usuario (cuenta, tipo, clave, dinero, nombre, apellido) 
+  VALUES (p_cuenta, p_tipo, p_clave, p_dinero, p_nombre, p_apellido);
   IF FOUND THEN
     RAISE NOTICE 'Usuario insertado exitosamente: % %', p_nombre, p_apellido;
     RETURN 1;
